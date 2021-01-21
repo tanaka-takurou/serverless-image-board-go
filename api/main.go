73d8aa65b3baee35a5e0472d7dc154660d1e3aa2
tanaka.takurou <tanaka.takurou@icloud.com>
2021-01-21 12:46:40 +0900

    fix for aws-sdk-go-v2 v1.0.0

--- api/main.go
+++ api/main.go
@@ -16,24 +16,26 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/expression"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbattribute"
+	dynamodbtypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
+	s3manager "github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 )
 
 type ImgData struct {
-	Img_Id  int    `json:"img_id"`
-	Status  int    `json:"status"`
-	Url     string `json:"url"`
-	Updated string `json:"updated"`
+	Img_Id  int    `dynamodbav:"img_id"`
+	Status  int    `dynamodbav:"status"`
+	Url     string `dynamodbav:"url"`
+	Updated string `dynamodbav:"updated"`
 }
 
 type TokenData struct {
-	Token     string `json:"token"`
-	Created   string `json:"created"`
+	Token     string `dynamodbav:"token"`
+	Created   string `dynamodbav:"created"`
 }
 
 type TokenResponse struct {
@@ -42,7 +44,6 @@ type TokenResponse struct {
 
 type Response events.APIGatewayProxyResponse
 
-var cfg aws.Config
 var dynamodbClient *dynamodb.Client
 
 const layout       string = "2006-01-02 15:04"
@@ -94,7 +95,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 func scan(ctx context.Context, tableName string, filt expression.ConditionBuilder, proj expression.ProjectionBuilder)(*dynamodb.ScanOutput, error)  {
 	if dynamodbClient == nil {
-		dynamodbClient = dynamodb.New(cfg)
+		dynamodbClient = dynamodb.NewFromConfig(getConfig(ctx))
 	}
 	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
 	if err != nil {
@@ -107,21 +108,19 @@ func scan(ctx context.Context, tableName string, filt expression.ConditionBuilde
 		ProjectionExpression:      expr.Projection(),
 		TableName:                 aws.String(tableName),
 	}
-	req := dynamodbClient.ScanRequest(input)
-	res, err := req.Send(ctx)
-	return res.ScanOutput, err
+	res, err := dynamodbClient.Scan(ctx, input)
+	return res, err
 }
 
-func put(ctx context.Context, tableName string, av map[string]dynamodb.AttributeValue) error {
+func put(ctx context.Context, tableName string, av map[string]dynamodbtypes.AttributeValue) error {
 	if dynamodbClient == nil {
-		dynamodbClient = dynamodb.New(cfg)
+		dynamodbClient = dynamodb.NewFromConfig(getConfig(ctx))
 	}
 	input := &dynamodb.PutItemInput{
 		Item:      av,
 		TableName: aws.String(tableName),
 	}
-	req := dynamodbClient.PutItemRequest(input)
-	_, err := req.Send(ctx)
+	_, err := dynamodbClient.PutItem(ctx, input)
 	return err
 }
 
@@ -131,7 +130,7 @@ func putToken(ctx context.Context, tokenTableName string, token string) error {
 		Token: token,
 		Created: t.Format(layout),
 	}
-	av, err := dynamodbattribute.MarshalMap(item)
+	av, err := attributevalue.MarshalMap(item)
 	if err != nil {
 		return err
 	}
@@ -142,28 +141,27 @@ func putToken(ctx context.Context, tokenTableName string, token string) error {
 	return nil
 }
 
-func get(ctx context.Context, tableName string, key map[string]dynamodb.AttributeValue, att string)(*dynamodb.GetItemOutput, error) {
+func get(ctx context.Context, tableName string, key map[string]dynamodbtypes.AttributeValue, att string)(*dynamodb.GetItemOutput, error) {
 	if dynamodbClient == nil {
-		dynamodbClient = dynamodb.New(cfg)
+		dynamodbClient = dynamodb.NewFromConfig(getConfig(ctx))
 	}
 	input := &dynamodb.GetItemInput{
 		TableName: aws.String(tableName),
 		Key: key,
 		AttributesToGet: []string{att},
 		ConsistentRead: aws.Bool(true),
-		ReturnConsumedCapacity: dynamodb.ReturnConsumedCapacityNone,
+		ReturnConsumedCapacity: dynamodbtypes.ReturnConsumedCapacityNone,
 	}
-	req := dynamodbClient.GetItemRequest(input)
-	res, err := req.Send(ctx)
-	return res.GetItemOutput, err
+	res, err := dynamodbClient.GetItem(ctx, input)
+	return res, err
 }
 
-func getImgCount(ctx context.Context, imgTableName string)(*int64, error)  {
+func getImgCount(ctx context.Context, imgTableName string)(int32, error)  {
 	filt := expression.NotEqual(expression.Name("status"), expression.Value(-1))
 	proj := expression.NamesList(expression.Name("img_id"), expression.Name("status"), expression.Name("url"), expression.Name("updated"))
 	result, err := scan(ctx, imgTableName, filt, proj)
 	if err != nil {
-		return nil, err
+		return int32(0), err
 	}
 	return result.ScannedCount, nil
 }
@@ -175,12 +173,12 @@ func putImg(ctx context.Context, imgTableName string, url string) error {
 		return err
 	}
 	item := ImgData {
-		Img_Id: int(*count) + 1,
+		Img_Id: int(count) + 1,
 		Status: 0,
 		Url: url,
 		Updated: t.Format(layout),
 	}
-	av, err := dynamodbattribute.MarshalMap(item)
+	av, err := attributevalue.MarshalMap(item)
 	if err != nil {
 		return err
 	}
@@ -192,8 +190,8 @@ func putImg(ctx context.Context, imgTableName string, url string) error {
 }
 
 func checkToken(ctx context.Context, tokenTableName string, token string) bool {
-	item := struct {Token string `json:"token"`}{token}
-	av, err := dynamodbattribute.MarshalMap(item)
+	item := struct {Token string `dynamodbav:"token"`}{token}
+	av, err := attributevalue.MarshalMap(item)
 	if err != nil {
 		return false
 	}
@@ -204,27 +202,26 @@ func checkToken(ctx context.Context, tokenTableName string, token string) bool {
 	return false
 }
 
-func delete(ctx context.Context, tableName string, key map[string]dynamodb.AttributeValue) error {
+func delete(ctx context.Context, tableName string, key map[string]dynamodbtypes.AttributeValue) error {
 	if dynamodbClient == nil {
-		dynamodbClient = dynamodb.New(cfg)
+		dynamodbClient = dynamodb.NewFromConfig(getConfig(ctx))
 	}
 	input := &dynamodb.DeleteItemInput{
 		TableName: aws.String(tableName),
 		Key: key,
 	}
 
-	req := dynamodbClient.DeleteItemRequest(input)
-	_, err := req.Send(ctx)
+	_, err := dynamodbClient.DeleteItem(ctx, input)
 	return err
 }
 
 func deleteToken(ctx context.Context, tokenTableName string, token string) error {
-	key := map[string]dynamodb.AttributeValue{
-		"token": {
-			S: aws.String(token),
-		},
+	item := struct {Token string `dynamodbav:"token"`}{token}
+	av, err := attributevalue.MarshalMap(item)
+	if err != nil {
+		return err
 	}
-	err := delete(ctx, tokenTableName, key)
+	err = delete(ctx, tokenTableName, av)
 	if err != nil {
 		return err
 	}
@@ -255,9 +252,9 @@ func uploadImage(ctx context.Context, imgTableName string, bucketName string, fi
 		return errors.New("this extension is invalid")
 	}
 	filename_ := string([]rune(filename)[:(len(filename) - len(extension))]) + t.Format(layout2) + extension
-	uploader := s3manager.NewUploader(cfg)
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		ACL: s3.ObjectCannedACLPublicRead,
+	uploader := s3manager.NewUploader(s3.NewFromConfig(getConfig(ctx)))
+	_, err = uploader.Upload(ctx, &s3.PutObjectInput{
+		ACL: s3types.ObjectCannedACLPublicRead,
 		Bucket: aws.String(bucketName),
 		Key: aws.String(filename_),
 		Body: bytes.NewReader(data),
@@ -271,13 +268,13 @@ func uploadImage(ctx context.Context, imgTableName string, bucketName string, fi
 	return nil
 }
 
-func init() {
+func getConfig(ctx context.Context) aws.Config {
 	var err error
-	cfg, err = external.LoadDefaultAWSConfig()
-	cfg.Region = os.Getenv("REGION")
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(os.Getenv("REGION")))
 	if err != nil {
 		log.Print(err)
 	}
+	return cfg
 }
 
 func main() {
